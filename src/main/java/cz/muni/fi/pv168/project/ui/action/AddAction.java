da793422210659db3fa6dce12775e6e624a93031
Michal Pataky <xpataky@fi.muni.cz>
2023-10-06 23:22:47 +0200

    fixed more imports

--- src/main/java/cz/muni/fi/pv168/project/ui/action/AddAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/AddAction.java
@@ -2,7 +2,8 @@ package cz.muni.fi.pv168.project.ui.action;
 
 import cz.muni.fi.pv168.project.ui.resources.Icons;
 
-import javax.swing.*;
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 

57325d45d59aabef20ce6b725ccf7b2e96c0d4a6
Michal Pataky <xpataky@fi.muni.cz>
2023-11-09 23:08:27 +0100

    Added functionality into Import/Export action but not in MainWindow

--- src/main/java/cz/muni/fi/pv168/project/ui/action/ExportAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/ExportAction.java
@@ -1,15 +1,25 @@
 package cz.muni.fi.pv168.project.ui.action;
 
+import cz.muni.fi.pv168.project.business.service.export.ExportService;
 import cz.muni.fi.pv168.project.ui.resources.Icons;
+import cz.muni.fi.pv168.project.util.Filter;
 
-import javax.swing.AbstractAction;
-import javax.swing.KeyStroke;
+import javax.swing.*;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 public class ExportAction extends AbstractAction {
-    public ExportAction() {
+
+    private final Component parent;
+    private final ExportService exportService;
+
+    public ExportAction(Component parent, ExportService exportService) {
         super("Export", Icons.EXPORT_ICON);
+        this.parent = parent;
+        this.exportService = exportService;
+
         putValue(SHORT_DESCRIPTION, "Exports data");
         putValue(MNEMONIC_KEY, KeyEvent.VK_E);
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl E"));
@@ -17,6 +27,21 @@ public class ExportAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO: Add actual performance to the exportAction
+        var fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        exportService.getFormats().forEach(f -> fileChooser.addChoosableFileFilter(new Filter(f)));
+
+        int dialogResult = fileChooser.showSaveDialog(parent);
+        if (dialogResult == JFileChooser.APPROVE_OPTION) {
+            String exportFile = fileChooser.getSelectedFile().getAbsolutePath();
+            var filter = fileChooser.getFileFilter();
+            if (filter instanceof Filter) {
+                exportFile = ((Filter) filter).decorate(exportFile);
+            }
+
+            exportService.exportData(exportFile);
+
+            JOptionPane.showMessageDialog(parent, "Export has successfully finished.");
+        }
     }
 }
--- src/main/java/cz/muni/fi/pv168/project/ui/action/ImportAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/ImportAction.java
@@ -1,15 +1,26 @@
 package cz.muni.fi.pv168.project.ui.action;
 
+import cz.muni.fi.pv168.project.business.service.export.ImportService;
 import cz.muni.fi.pv168.project.ui.resources.Icons;
 
-import javax.swing.AbstractAction;
-import javax.swing.KeyStroke;
+import javax.swing.*;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 public class ImportAction extends AbstractAction {
-    public ImportAction() {
+
+    private final Component parent;
+    private final ImportService importService;
+    private final Runnable callback;
+
+    public ImportAction(Component parent, ImportService importService, Runnable callback) {
         super("Import", Icons.IMPORT_ICON);
+        this.parent = parent;
+        this.importService = importService;
+        this.callback = callback;
+
         putValue(SHORT_DESCRIPTION, "Imports data");
         putValue(MNEMONIC_KEY, KeyEvent.VK_I);
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl I"));
@@ -17,6 +28,19 @@ public class ImportAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO: Add actual performance to the exportAction
+        var fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        importService.getFormats().forEach(f -> fileChooser.addChoosableFileFilter(new Filter(f)));
+
+        int dialogResult = fileChooser.showOpenDialog(parent);
+
+        if (dialogResult == JFileChooser.APPROVE_OPTION) {
+            File importFile = fileChooser.getSelectedFile();
+
+            importService.importData(importFile.getAbsolutePath());
+
+            callback.run();
+            JOptionPane.showMessageDialog(parent, "Import was done");
+        }
     }
 }

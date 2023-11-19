b8512ecaafa4a211dad59a3d05a435a770178af3
Michal Pataky <xpataky@fi.muni.cz>
2023-11-19 09:11:53 +0100

    Javadoc for EntityMatcher

--- src/main/java/cz/muni/fi/pv168/project/ui/filters/matchers/EntityMatcher.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/filters/matchers/EntityMatcher.java
@@ -12,6 +12,14 @@ import javax.swing.RowFilter;
  */
 public abstract class EntityMatcher<T> extends RowFilter<EntityTableModel<T>, Integer> {
 
+    /**
+     * Determines whether a particular row should be included in the filtered view.
+     *
+     * @param entry The entry representing the row in the table model.
+     * @return {@code true} if the row should be included, {@code false} otherwise.
+     *
+     * @throws IndexOutOfBoundsException If the entry identifier is out of bounds in the table model.
+     */
     @Override
     public boolean include(Entry<? extends EntityTableModel<T>, ? extends Integer> entry) {
         EntityTableModel<T> tableModel = entry.getModel();

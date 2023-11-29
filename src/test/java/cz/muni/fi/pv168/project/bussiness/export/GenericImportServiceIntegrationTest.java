3c6450c8d3bf8d54dc9decac16a0a2a95a308bd3
Albert Koziar <509501@mail.muni.cz>
2023-11-29 17:49:16 +0100

    GenericImportServiceIntegrationTest class now functional (need to add GUID to json processing), StringLengthValidatorTest also functional

--- src/test/java/cz/muni/fi/pv168/project/bussiness/export/GenericImportServiceIntegrationTest.java
+++ src/test/java/cz/muni/fi/pv168/project/bussiness/export/GenericImportServiceIntegrationTest.java
@@ -1,147 +1,142 @@
-//package cz.muni.fi.pv168.project.bussiness.export;
-//
-//import cz.muni.fi.pv168.employees.business.model.Department;
-//import cz.muni.fi.pv168.employees.business.model.Employee;
-//import cz.muni.fi.pv168.employees.business.model.Gender;
-//import cz.muni.fi.pv168.employees.business.model.UuidGuidProvider;
-//import cz.muni.fi.pv168.employees.business.service.crud.DepartmentCrudService;
-//import cz.muni.fi.pv168.employees.business.service.crud.EmployeeCrudService;
-//import cz.muni.fi.pv168.employees.business.service.crud.EntityAlreadyExistsException;
-//import cz.muni.fi.pv168.employees.business.service.validation.DepartmentValidator;
-//import cz.muni.fi.pv168.employees.business.service.validation.EmployeeValidator;
-//import cz.muni.fi.pv168.employees.business.service.validation.ValidationException;
-//import cz.muni.fi.pv168.employees.export.csv.BatchCsvImporter;
-//import cz.muni.fi.pv168.employees.storage.memory.InMemoryRepository;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.time.LocalDate;
-//import java.util.List;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-//
-///**
-// * Integration tests for the {@link GenericImportService}
-// *
-// * @author Vojtech Sassmann
-// * @since 1.0.0
-// */
-//class GenericImportServiceIntegrationTest
-//{
-//    private static final Path PROJECT_ROOT = Paths.get(System.getProperty("project.basedir", "")).toAbsolutePath();
-//    private static final Path TEST_RESOURCES = PROJECT_ROOT.resolve(Path.of("src", "test", "resources"));
-//
-//    private EmployeeCrudService employeeCrudService;
-//    private GenericImportService genericImportService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        var employeeRepository = new InMemoryRepository<Employee>(List.of());
-//        var employeeValidator = new EmployeeValidator();
-//        var uuidGuidProvider = new UuidGuidProvider();
-//        employeeCrudService = new EmployeeCrudService(employeeRepository, employeeValidator, uuidGuidProvider);
-//
-//        var departmentRepository = new InMemoryRepository<Department>(List.of());
-//        var departmentValidator = new DepartmentValidator();
-//        var departmentCrudService = new DepartmentCrudService(departmentRepository, departmentValidator,
-//                uuidGuidProvider);
-//
-//        genericImportService = new GenericImportService(
-//                employeeCrudService,
-//                departmentCrudService,
-//                List.of(new BatchCsvImporter())
-//        );
-//    }
-//
-//    @Test
-//    void importNoEmployees() {
-//        Path importFilePath = TEST_RESOURCES.resolve("empty.csv");
-//        genericImportService.importData(importFilePath.toString());
-//
-//        assertThat(employeeCrudService.findAll())
-//                .isEmpty();
-//    }
-//
-//    @Test
-//    void singleEmployee() {
-//        Path importFilePath = TEST_RESOURCES.resolve("single-employee.csv");
-//        genericImportService.importData(importFilePath.toString());
-//
-//        // We need to specify the 'usingRecursiveFieldByFieldElementComparator' to actually compare
-//        // all fields of the Employee and all fields of the Department. If we don't specify this comparator,
-//        // comparison is done by using the 'equals' method. The 'equals' method compares only the 'guid' fields.
-//        assertThat(employeeCrudService.findAll())
-//                .usingRecursiveFieldByFieldElementComparator()
-//                .containsExactly(
-//                        new Employee(
-//                                "76c59af3-6e9a-4fcb-bd7e-d0a163ed8b45",
-//                                "John",
-//                                "Doe",
-//                                Gender.MALE,
-//                                LocalDate.of(1990, 12, 12),
-//                                new Department(
-//                                        "99605972-dc16-4ca1-9fa3-987b7da996f4",
-//                                        "IT",
-//                                        "044"
-//                                )
-//                        )
-//                );
-//    }
-//
-//    @Test
-//    void multipleEmployees() {
-//        Path importFilePath = TEST_RESOURCES.resolve("multiple-employees.csv");
-//        genericImportService.importData(importFilePath.toString());
-//
-//        var department = new Department(
-//                "99605972-dc16-4ca1-9fa3-987b7da996f4",
-//                "IT",
-//                "044"
-//        );
-//        assertThat(employeeCrudService.findAll())
-//                .usingRecursiveFieldByFieldElementComparator()
-//                .containsExactlyInAnyOrder(
-//                        new Employee(
-//                                "76c59af3-6e9a-4fcb-bd7e-d0a163ed8b45",
-//                                "John",
-//                                "Doe",
-//                                Gender.MALE,
-//                                LocalDate.of(1990, 12, 12),
-//                                department
-//                        ),
-//                        new Employee(
-//                                "dc96a827-b56b-4252-bf24-bb8f25209f3e",
-//                                "Ellie",
-//                                "Williams",
-//                                Gender.FEMALE,
-//                                LocalDate.of(1992, 6, 12),
-//                                department
-//                        )
-//                );
-//    }
-//
-//    @Test
-//    void invalidFirstNameFails() {
-//        Path importFilePath = TEST_RESOURCES.resolve("invalid-firstname-employee.csv");
-//
-//        var stringPath = importFilePath.toString();
-//        // we want only single invocation in the 'isThrownBy' method so that we don't accidentally catch
-//        // unwanted exceptions
-//        assertThatExceptionOfType(ValidationException.class)
-//                .isThrownBy(() -> genericImportService.importData(stringPath))
-//                .withMessageContaining("'First name' length is not between 2 (inclusive) and 150 (inclusive)");
-//    }
-//
-//    @Test
-//    void duplicateGuidFails() {
-//        Path importFilePath = TEST_RESOURCES.resolve("duplicate-guid-employees.csv");
-//
-//        var stringPath = importFilePath.toString();
-//        // we want only single invocation in the 'isThrownBy' method so that we don't accidentally catch
-//        // unwanted exceptions
-//        assertThatExceptionOfType(EntityAlreadyExistsException.class)
-//                .isThrownBy(() -> genericImportService.importData(stringPath))
-//                .withMessage("Employee with given guid already exists: 76c59af3-6e9a-4fcb-bd7e-d0a163ed8b45");
-//    }
-//}
+package cz.muni.fi.pv168.project.bussiness.export;
+
+
+import cz.muni.fi.pv168.project.business.model.Ingredient;
+import cz.muni.fi.pv168.project.business.model.Recipe;
+import cz.muni.fi.pv168.project.business.model.RecipeIngredient;
+import cz.muni.fi.pv168.project.business.model.Unit;
+import cz.muni.fi.pv168.project.business.model.UuidGuidProvider;
+import cz.muni.fi.pv168.project.business.service.crud.EntityAlreadyExistsException;
+import cz.muni.fi.pv168.project.business.service.crud.IngredientCrudService;
+import cz.muni.fi.pv168.project.business.service.crud.RecipeCrudService;
+import cz.muni.fi.pv168.project.business.service.crud.UnitCrudService;
+import cz.muni.fi.pv168.project.business.service.import_export.GenericImportService;
+import cz.muni.fi.pv168.project.business.service.import_export.format.BatchJsonImporter;
+import cz.muni.fi.pv168.project.business.service.validation.IngredientValidator;
+import cz.muni.fi.pv168.project.business.service.validation.RecipeValidator;
+import cz.muni.fi.pv168.project.business.service.validation.UnitValidator;
+import cz.muni.fi.pv168.project.business.service.validation.ValidationException;
+import cz.muni.fi.pv168.project.storage.InMemoryRepository;
+import cz.muni.fi.pv168.project.ui.model.IngredientTableModel;
+import cz.muni.fi.pv168.project.ui.model.RecipeTableModel;
+import cz.muni.fi.pv168.project.ui.model.UnitTableModel;
+import cz.muni.fi.pv168.project.ui.model.enums.RecipeCategory;
+import cz.muni.fi.pv168.project.ui.model.enums.UnitType;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * Integration tests for the {@link GenericImportService}
+ */
+class GenericImportServiceIntegrationTest {
+    private static final Path PROJECT_ROOT = Paths.get(System.getProperty("project.basedir", "")).toAbsolutePath();
+    private static final Path TEST_RESOURCES = PROJECT_ROOT.resolve(Path.of("src", "test", "resources"));
+
+    private GenericImportService genericImportService;
+    private RecipeCrudService recipeCrudService;
+
+    @BeforeEach
+    void setUp() {
+        var recipeRepository = new InMemoryRepository<Recipe>(List.of());
+        var recipeValidator = new RecipeValidator();
+        var uuidGuidProvider = new UuidGuidProvider();
+        recipeCrudService = new RecipeCrudService(recipeRepository, recipeValidator, uuidGuidProvider);
+
+        var ingredientRepository = new InMemoryRepository<Ingredient>(List.of());
+        var ingredientValidator = new IngredientValidator();
+        var ingredientCrudService = new IngredientCrudService(ingredientRepository, ingredientValidator,
+                uuidGuidProvider);
+
+        var unitRepository = new InMemoryRepository<Unit>(List.of());
+        var unitValidator = new UnitValidator();
+        var unitCrudService = new UnitCrudService(unitRepository, unitValidator,
+                uuidGuidProvider);
+
+        RecipeTableModel recipeTableModel = new RecipeTableModel(recipeCrudService);
+        IngredientTableModel ingredientTableModel = new IngredientTableModel(ingredientCrudService);
+        UnitTableModel unitTableModel = new UnitTableModel(unitCrudService);
+        genericImportService = new GenericImportService(
+                recipeTableModel,
+                unitTableModel,
+                ingredientTableModel,
+                List.of(new BatchJsonImporter())
+        );
+    }
+
+    @Test
+    void importNoRecipes() {
+        Path importFilePath = TEST_RESOURCES.resolve("empty.json");
+        genericImportService.importData(importFilePath.toString());
+
+        assertThat(recipeCrudService.findAll())
+                .isEmpty();
+    }
+
+    @Test
+    void singleRecipe() {
+        Path importFilePath = TEST_RESOURCES.resolve("single-recipe.json");
+        genericImportService.importData(importFilePath.toString());
+
+        // We need to specify the 'usingRecursiveFieldByFieldElementComparator' to actually compare
+        // all fields of the Employee and all fields of the Department. If we don't specify this comparator,
+        // comparison is done by using the 'equals' method. The 'equals' method compares only the 'guid' fields.
+
+        assertThat(recipeCrudService.findAll())
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactly(
+                        new Recipe( "76c59af3-6e9a-4fcb-bd7e-d0a163ed8b45", "Krtkův dort", RecipeCategory.ZAKUSEK, LocalTime.NOON, 2,
+                                new ArrayList<>(),"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+                );
+    }
+
+    @Test
+    void multipleRecipes() {
+        Path importFilePath = TEST_RESOURCES.resolve("multiple-recipes.json");
+        genericImportService.importData(importFilePath.toString());
+
+        ArrayList<RecipeIngredient> ingredients = new ArrayList<>();
+        ingredients.add(new RecipeIngredient("Vlašák", 1000000, new Unit("If I see one more type fail im gonna lose it", "kilogram", UnitType.Weight, 0.001), 1));
+
+        assertThat(recipeCrudService.findAll())
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrder(
+                        new Recipe( "Krtkův dort","AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", RecipeCategory.ZAKUSEK, LocalTime.NOON, 2,
+                                ingredients, 1),
+                        new Recipe( "Chleba s vlašákem","AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", RecipeCategory.ZAKUSEK, LocalTime.NOON, 2,
+                                ingredients, 1)
+                );
+    }
+
+    @Test
+    void invalidNameFails() {
+        Path importFilePath = TEST_RESOURCES.resolve("invalid-name-recipe.json");
+
+        var stringPath = importFilePath.toString();
+        // we want only single invocation in the 'isThrownBy' method so that we don't accidentally catch
+        // unwanted exceptions
+        assertThatExceptionOfType(ValidationException.class)
+                .isThrownBy(() -> genericImportService.importData(stringPath))
+                .withMessageContaining("'Recipe name' length is not between 2 (inclusive) and 150 (inclusive)");
+    }
+
+    @Test
+    void duplicateGuidFails() {
+        Path importFilePath = TEST_RESOURCES.resolve("duplicate-guid-recipes.json");
+
+        var stringPath = importFilePath.toString();
+        // we want only single invocation in the 'isThrownBy' method so that we don't accidentally catch
+        // unwanted exceptions
+        assertThatExceptionOfType(EntityAlreadyExistsException.class)
+                .isThrownBy(() -> genericImportService.importData(stringPath))
+                .withMessage("Recipe with given guid already exists: 76c59af3-6e9a-4fcb-bd7e-d0a163ed8b45");
+    }
+}
---
+++ src/test/java/cz/muni/fi/pv168/project/bussiness/validation/StringLengthValidatorTest.java
@@ -0,0 +1,58 @@
+package cz.muni.fi.pv168.project.bussiness.validation;
+
+import cz.muni.fi.pv168.project.business.service.validation.ValidationResult;
+import cz.muni.fi.pv168.project.business.service.validation.common.StringLengthValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for {@link StringLengthValidator}
+ */
+class StringLengthValidatorTest {
+
+    private StringLengthValidator stringLengthValidator;
+
+    @BeforeEach
+    void setUp() {
+        stringLengthValidator = new StringLengthValidator(2, 10);
+    }
+
+    @Test
+    void validateSuccess() {
+        var result = stringLengthValidator.validate("test");
+
+        assertThat(result).isEqualTo(ValidationResult.success());
+    }
+
+    @Test
+    void validateLowerBoundFail() {
+        var result = stringLengthValidator.validate("t");
+
+        assertThat(result).isEqualTo(ValidationResult
+                .failed("'t' length is not between 2 (inclusive) and 10 (inclusive)"));
+    }
+
+    @Test
+    void validateUpperBoundFail() {
+        var result = stringLengthValidator.validate("xxxxxxxxxxx");
+
+        assertThat(result).isEqualTo(ValidationResult
+                .failed("'xxxxxxxxxxx' length is not between 2 (inclusive) and 10 (inclusive)"));
+    }
+
+    @Test
+    void validateLowerBoundSuccess() {
+        var result = stringLengthValidator.validate("te");
+
+        assertThat(result).isEqualTo(ValidationResult.success());
+    }
+
+    @Test
+    void validateUpperBoundSuccess() {
+        var result = stringLengthValidator.validate("xxxxxxxxxx");
+
+        assertThat(result).isEqualTo(ValidationResult.success());
+    }
+}

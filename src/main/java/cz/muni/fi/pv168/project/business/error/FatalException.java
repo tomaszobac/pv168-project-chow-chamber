2dc95ff7b9d9088b2d45863329dde2874b6fdaac
Michal Pataky <xpataky@fi.muni.cz>
2023-12-13 12:08:47 +0100

    FatalError is now FatalException

--- src/main/java/cz/muni/fi/pv168/project/business/error/FatalException.java
+++ src/main/java/cz/muni/fi/pv168/project/business/error/FatalException.java
@@ -3,5 +3,5 @@ package cz.muni.fi.pv168.project.business.error;
 /**
  * Marker interfaces used to designate exception as unrecoverable
  */
-public interface FatalError extends ApplicationException {
+public interface FatalException extends ApplicationException {
 }

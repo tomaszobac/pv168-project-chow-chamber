1c9f8245a601b6b972bab2482978147a99c45d8f
Michal Pataky <xpataky@fi.muni.cz>
2023-12-13 12:08:47 +0100

    FatalError is now FatalException

--- src/main/java/cz/muni/fi/pv168/project/storage/sql/dao/FatalDataStorageException.java
+++ src/main/java/cz/muni/fi/pv168/project/storage/sql/dao/FatalDataStorageException.java
@@ -1,9 +1,10 @@
 package cz.muni.fi.pv168.project.storage.sql.dao;
 
-import cz.muni.fi.pv168.project.business.error.FatalError;
+import cz.muni.fi.pv168.project.business.error.FatalException;
+
 import java.io.Serial;
 
-public class FatalDataStorageException extends DataStorageException implements FatalError {
+public class FatalDataStorageException extends DataStorageException implements FatalException {
 
     @Serial
     private static final long serialVersionUID = 4512050973972830440L;
